84de18f73660f3066abdcd393d85efc3a8e1b1dd
David Ramos <ramos@cs.stanford.edu>
2019-07-02 16:23:09 -0700

    Remove FnBox (#7)
    Support latest nightly after
    https://github.com/rust-lang/rust/pull/62043 was merged.

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,5 @@
 //! Support module for `#[datatest::data(..)]`
 use serde::de::DeserializeOwned;
-use std::boxed::FnBox;
 use test::TDynBenchFn;
 use yaml_rust::parser::Event;
 use yaml_rust::scanner::Marker;
@@ -17,8 +16,8 @@ pub struct DataTestDesc {
 /// Used internally for `#[datatest::data(..)]` tests.
 #[doc(hidden)]
 pub enum DataTestFn {
-    TestFn(Box<FnBox() + Send + 'static>),
-    BenchFn(Box<TDynBenchFn + 'static>),
+    TestFn(Box<dyn FnOnce() + Send + 'static>),
+    BenchFn(Box<dyn TDynBenchFn + 'static>),
 }
 
 /// Descriptor of the data test case where the type of the test case data is `T`.
