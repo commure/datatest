9b4fafb71db9343a140fa20d712f22acb1f3ff8b
Ivan Dubrov <ivan@commure.com>
2019-09-21 22:07:48 -0700

    ❓Try only doing write protection

--- tests/datatest_stable.rs
+++ tests/datatest_stable.rs
@@ -1,4 +1,4 @@
-#[cfg(feature = "stable")]
+#[cfg(feature = "subvert_stable_guarantees")]
 
 // We want to share tests between "nightly" and "stable" suites. These have to be two different
 // suites as we set `harness = false` for the "stable" one.
