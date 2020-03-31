d05d1ca25c7238380fe66168612c9fa1ef7bca29
Preston TW <preston_tunnell_wilson@brown.edu>
2020-03-31 18:47:27 -0400

    Add os tag to another test

--- tests/bench.rs
+++ tests/bench.rs
@@ -31,6 +31,7 @@ use test::Bencher;
   output = r"${1}.output.txt",
 })]
 #[bench]
+#[cfg(unix)]
 fn files_test_strings(bencher: &mut Bencher, input: &str, output: &str) {
     bencher.iter(|| {
         assert_eq!(format!("Hello, {}!", input), output);
